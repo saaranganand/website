cccab96b9320e0094c20c22a942e85ac92eedffe
Saarang Anand <saaranganand2013@gmail.com>
2026-04-14 23:06:38 -0700

    hero content

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,31 @@
+import memojiImage from '@/assets/images/memoji-computer.png'
+import Image from 'next/image'
+import ArrowDown from '@/assets/icons/arrow-down.svg';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+  <div>
+    <div>
+      <Image src={memojiImage} alt="Person peeking from behind laptop" />
+      <div>
+        <div></div>
+        <div>Available for new projects</div>
+      </div>
+      <h1>Building Exceptional User Interfaces</h1>
+      <p>
+        I specialize in transforming designs into functional, high-performing web applications. Let's discuss your next project.
+      </p>
+      <div>
+        <button>
+          <span>Explore My Work</span>
+          <ArrowDown />
+        </button>
+        <button>
+          <span>👋</span>
+          <span>Let's Connect</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  );
 };
