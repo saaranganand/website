3c17e941cf3d2c6e9f405fc6c909736c995d8c71
Saarang Anand <saaranganand2013@gmail.com>
2026-05-05 13:34:08 -0700

    tape animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,8 @@ const config: Config = {
         serif: 'var(--font-serif)'
       },
       animation: {
-        'ping-large': "ping-large 1s ease-in-out infinite"
+        'ping-large': "ping-large 1s ease-in-out infinite",
+        'move-left': 'move-left 1s linear infinite',
       },
       keyframes: {
         'ping-large': {
@@ -34,9 +35,17 @@ const config: Config = {
             transform: 'scale(3)',
             opacity: '0'
           }
+        },
+        'move-left': {
+          '0%': {
+            transform: 'translateX(0%)'
+          },
+          '100%': {
+            transform: 'translateX(-50%)'
+          }
         }
       }
-    },
+    }
   },
   plugins: [],
 };
